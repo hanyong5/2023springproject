2b7bf7af3925e17dac79c92f55f0cc69ab20d2ff
hanyong5 <hanyong5@naver.com>
2023-12-14 11:21:08 +0900

    user entity

--- Lec19_Jwt01/src/main/java/com/study/springboot/config/WebSecurityConfig.java
+++ Lec19_Jwt01/src/main/java/com/study/springboot/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,4 +30,11 @@ public class WebSecurityConfig {
         .anyRequest().permitAll();
 		return http.build();
 	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
 }
