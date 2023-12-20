092984b05675b11959cd7af2fad846f24630ba6d
hanyong5 <hanyong5@naver.com>
2023-12-20 11:49:37 +0900

    fileupload

--- Lec20_FileUpload/src/main/java/com/study/springboot/api/FileDataApiController.java
+++ Lec20_FileUpload/src/main/java/com/study/springboot/api/FileDataApiController.java
@@ -1,15 +1,20 @@
 package com.study.springboot.api;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.study.springboot.entity.FileData;
 import com.study.springboot.service.FileDataService;
 
 import lombok.RequiredArgsConstructor;
@@ -27,4 +32,30 @@ public class FileDataApiController {
 		String uploadImage= fileDataService.uploadImageSystem(file);
 		return ResponseEntity.status(HttpStatus.OK).body(uploadImage);
 	}
+	
+	@GetMapping("/file/{id}")
+	@CrossOrigin
+	public ResponseEntity<?> downImage(@PathVariable("id") Long id) throws IOException{
+		byte[] downloadImage = fileDataService.downloadImageSystem(id);
+		if(downloadImage != null) {
+			return ResponseEntity.status(HttpStatus.OK)
+					.contentType(MediaType.valueOf("image/png"))
+					.body(downloadImage);
+		}else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+	
+	@GetMapping("/fileList")
+	@CrossOrigin
+	public ResponseEntity<List<FileData>> getFileListdata(){
+		List<FileData> fileDataList = fileDataService.findAll();
+		
+		if(fileDataList != null) {
+			return ResponseEntity.status(HttpStatus.OK).body(fileDataList);
+		}else {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
+	}
+	
 }
